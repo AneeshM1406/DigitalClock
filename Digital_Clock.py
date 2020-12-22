e9df2a985d65ce8f2fd9aa7fd1af864a66effa72
AneeshM1406 <76500442+AneeshM1406@users.noreply.github.com>
2020-12-22 19:44:07 -0400

    Update Digital_Clock.py

--- Digital_Clock.py
+++ Digital_Clock.py
@@ -9,7 +9,7 @@ def call_time():
     label.config(text = string) 
     label.after(1000, call_time) 
   
-label = Label(app, font = ('calibri', 40, 'bold'), bg = 'blue', fg = 'white') 
+label = Label(app, font = ('arial', 40, 'bold'), bg = 'blue', fg = 'white') 
 label.pack(anchor = 'center') 
 
 call_time() 
