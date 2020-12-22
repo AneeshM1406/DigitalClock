5fe25b98b2d12a92013b03cd2399cbdc2aef7b16
AneeshM1406 <76500442+AneeshM1406@users.noreply.github.com>
2020-12-22 19:42:29 -0400

    Add files via upload

---
+++ Digital_Clock.py
@@ -0,0 +1,17 @@
+from tkinter import Label, Tk  
+from time import strftime 
+  
+app = Tk() 
+app.title('Clock') 
+
+def call_time(): 
+    string = strftime('%I:%M:%S %p') 
+    label.config(text = string) 
+    label.after(1000, call_time) 
+  
+label = Label(app, font = ('calibri', 40, 'bold'), bg = 'blue', fg = 'white') 
+label.pack(anchor = 'center') 
+
+call_time() 
+  
+app.mainloop()
